6c74b48490cfc0941af559a3db731cdf93a19b27
CodeClimberNT <nicktaormina3@gmail.com>
2024-04-11 09:03:18 +0200

    Lab 2 Added syscalls: read write exit

---
+++ kern/compile/SYS-OPT/.depend.read_syscalls.c
@@ -0,0 +1,6 @@
+read_syscalls.o: ../../syscall/read_syscalls.c ../../include/types.h \
+ ../../include/kern/types.h includelinks/kern/machine/types.h \
+ includelinks/machine/types.h ../../include/kern/unistd.h \
+ ../../include/clock.h ../../include/kern/time.h \
+ ../../include/copyinout.h ../../include/syscall.h ../../include/cdefs.h \
+ ../../include/lib.h opt-noasserts.h
